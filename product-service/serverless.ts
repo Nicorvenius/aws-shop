4999faab183e17f71efd9709082b5b371d6f643d
Ncorvenius <nicorvenius@gmail.com>
2021-10-07 02:07:54 +0300

    feat: Create SNS with SQS

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -2,11 +2,12 @@ import type { AWS } from '@serverless/typescript';
 
 import getProductsList from '@functions/getProductsList';
 import getProductsById from '@functions/getProductsById';
-import createProduct from '@functions/createProduct';
 
 const serverlessConfiguration: AWS = {
   service: 'product-service',
   frameworkVersion: '2',
+  useDotenv: true,
+  configValidationMode: 'error',
   custom: {
     webpack: {
       webpackConfig: './webpack.config.js',
@@ -19,23 +20,110 @@ const serverlessConfiguration: AWS = {
     runtime: 'nodejs14.x',
     region: "eu-west-1",
     stage: "dev",
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: "sns:*",
+        Resource: [{ Ref: "createProductTopic" }],
+      },
+      {
+        Effect: "Allow",
+        Action: "sqs:*",
+        Resource: {
+          "Fn::GetAtt": ["catalogItemsQueue", "Arn"],
+        },
+      },
+    ],
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      PG_HOST: '${env:PG_HOST}',
-      PG_PORT: '${env:PG_PORT}',
-      PG_DB: '${env:PG_DB}',
-      PG_USER: '${env:PG_USER}',
-      PG_PASSWORD: '${env:PG_PASSWORD}',
+      PG_DATABASE: "${env:PG_DB}",
+      PG_USERNAME: "${env:PG_USER}",
+      PG_PASSWORD: "${env:PG_PASSWORD}",
+      FIRST_EMAIL: "${env:FIRST_EMAIL}",
+      SECOND_EMAIL: "${env:SECOND_EMAIL}",
+      SQS_URL: {
+        Ref: "catalogItemsQueue",
+      },
+      SNS_ARN: {
+        Ref: "createProductTopic",
+      },
     },
     lambdaHashingVersion: '20201221',
   },
+  resources: {
+    Resources: {
+      catalogItemsQueue: {
+        Type: "AWS::SQS::Queue",
+        Properties: {
+          QueueName: "catalogItemsQueue",
+        },
+      },
+      createProductTopic: {
+        Type: "AWS::SNS::Topic",
+        Properties: {
+          TopicName: "sqs-create-topic",
+        },
+      },
+      createProductSubscription: {
+        Type: "AWS::SNS::Subscription",
+        Properties: {
+          Endpoint: "${env:FIRST_EMAIL}",
+          Protocol: "email",
+          TopicArn: {
+            Ref: "createProductTopic",
+          },
+          FilterPolicy: {
+            price: [
+              {
+                numeric: ["<=", 5],
+              },
+            ],
+          },
+        },
+      },
+      expensiveProductSubscription: {
+        Type: "AWS::SNS::Subscription",
+        Properties: {
+          Endpoint: "${env:SECOND_EMAIL}",
+          Protocol: "email",
+          TopicArn: {
+            Ref: "createProductTopic",
+          },
+          FilterPolicy: {
+            price: [
+              {
+                numeric: [">", 5],
+              },
+            ],
+          },
+        },
+      },
+    },
+    Outputs: {
+      catalogItemsQueue: {
+        Value: {
+          Ref: "catalogItemsQueue",
+        },
+        Export: {
+          Name: "catalogItemsQueue",
+        },
+      },
+      catalogItemsQueueArn: {
+        Value: {
+          "Fn::GetAtt": ["catalogItemsQueue", "Arn"],
+        },
+        Export: {
+          Name: "catalogItemsQueueArn",
+        },
+      },
+    },
+  },
   // import the function via paths
-  functions: { getProductsList, getProductsById, createProduct },
-  useDotenv: true,
+  functions: { getProductsList, getProductsById },
 };
 
 module.exports = serverlessConfiguration;
